57d3a6610bc4dd4dd1ff6a78c29e448b2e8c49e1
Jesse Rodriguez <jesserod@cs.stanford.edu>
2014-12-28 00:46:49 -0800

    ignore client dir

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # This lets us run the server with auto-reloading after code changes
 # ./node_modules/forever/bin/forever index.js -w --watchDirectory `pwd`
-./node_modules/nodemon/bin/nodemon.js
+./node_modules/nodemon/bin/nodemon.js --ignore client
