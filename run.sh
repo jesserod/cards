86d6fcd7ff2c8462ceded0c897db7373457cd9a8
Jesse Rodriguez <jesserod@cs.stanford.edu>
2015-09-26 14:10:30 -0700

    usage for run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $1 == '-h' ]]; then
+  echo "usage: [port=1234] ./run.sh"
+  exit 1
+fi
+
 # This lets us run the server with auto-reloading after code changes
 # ./node_modules/forever/bin/forever index.js -w --watchDirectory `pwd`
 ./node_modules/nodemon/bin/nodemon.js --ignore client
